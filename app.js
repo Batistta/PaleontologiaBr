c682020133350ec434328ca7a9c0972300669d28
Fbattistta <154758609+Batistta@users.noreply.github.com>
2024-09-07 00:05:59 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,72 @@
+//posteriormente add mais imagens e tags para pesquisar por palavras chaves
+
+
+let entrada = document.getElementById("entrada");
+let resultado = document.getElementById("resultado");
+
+function capitalizeFirstLetter(str) {
+    if (str.length === 0) return str; // Retorna a string vazia se a entrada estiver vazia
+    return str.charAt(0).toUpperCase() + str.slice(1).toLowerCase();
+}
+
+function pesquisar() {
+    texto = entrada.value;
+    let info = "";
+    
+    for (let dado of dados) {
+        if(!texto) //campo vazio
+            {
+            console.log("Campo vazio");
+            info += "<h2>Insira algo para pesquisar</h2>"
+            break;
+        }
+        else{
+        if (dado.titulo.includes(capitalizeFirstLetter(entrada.value.trim()))) {
+            info += `     
+            <div class="item-resultado">
+                <h2>
+                    <a target="_blank" id="titulo" href="${dado.link}">${dado.titulo}</a>
+                </h2>
+                <p class="descricao-periodo" id="periodo">${dado.periodo}</p>
+        
+                <p class="descricao-meta" id="descricao">${dado.descricao}</p>
+                <img src="imagem-seres/${dado.imagem}" width="500"><br>
+                <a target="_blank" id="link" href="${dado.link}" class="saiba-mais">saiba mais</a>
+                
+            </div> `;
+        }
+        else if(dado.periodo.includes(texto) || dado.descricao.includes(texto)){
+            info += `     
+            <div class="item-resultado">
+                <h2>
+                    <a target="_blank" id="titulo" href="${dado.link}">${dado.titulo}</a>
+                </h2>
+                <p class="descricao-periodo" id="periodo">${dado.periodo}</p>
+        
+                <p class="descricao-meta" id="descricao">${dado.descricao}</p>
+                <img src="imagem-seres/${dado.imagem}" width="500"><br>
+                <a target="_blank" id="link" href="${dado.link}" class="saiba-mais">saiba mais</a>
+            </div> `;
+        }
+    }
+}
+    if(info == ""){
+        info += "<h2>termo nao encontrado</h2>";
+    }
+    resultado.innerHTML = info;
+}
+
+function sortear(){
+    let i = Math.round(Math.random() * (dados.length - 1) * 1)
+    console.log(i)
+    resultado.innerHTML = `<div class="item-resultado">
+    <h2>
+        <a target="_blank" id="titulo" href="${dados[i].link}">${dados[i].titulo}</a>
+    </h2>
+    <p class="descricao-periodo" id="periodo">${dados[i].periodo}</p>
+
+    <p class="descricao-meta" id="descricao">${dados[i].descricao}</p>
+    <img src="imagem-seres/${dado.imagem}" width="500"><br>
+    <a target="_blank" id="link" href="${dados[i].link}" class="saiba-mais">saiba mais</a>
+</div> `;
+}
---
+++ dados.js
@@ -0,0 +1,118 @@
+let dados = [
+    {
+        titulo: "Sigilmassaurus",
+        descricao: 'O Sigilmassasaurus é um Spinosauróideo Africano, cujo existem supostos registros desses na Ilha do Cajual, no Maranhão. O Sigilmassasaurus é considerado por alguns pesquisadores como um sinônimo de Spinosaurus assim como o Oxalaia.',
+        periodo: "110 - 95 MA. Cretáceo",
+        link: "https://pt.wikipedia.org/wiki/Sigilmassasaurus"
+    },
+    {
+        titulo: "Oxalaia",
+        descricao: 'Oxalaia é um gênero de dinossauros terópodes espinossaurídeos que viveram onde é hoje a Região Nordeste do Brasil durante o andar Cenomaniano do período Cretáceo Superior, há entre 100,5 e 93,9 milhões de anos. Seus únicos fósseis conhecidos foram encontrados em 1999 na Ilha do Cajual, nas rochas da Formação Alcântara, conhecida por sua abundância de espécimes fósseis fragmentados e isolados. Os restos de Oxalaia foram descritos em 2011 pelo paleontólogo brasileiro Alexander Kellner e colegas, que atribuíram os achados a um novo gênero contendo apenas uma espécie, Oxalaia quilombensis. O nome genérico refere-se à divindade africana Oxalá; já o descritor específico, aos assentamentos quilombolas brasileiros. Oxalaia quilombensis é a oitava espécie de terópode encontrada no Brasil e o maior dinossauro carnívoro ali descoberto. Está intimamente relacionado com o gênero africano Spinosaurus e/ou pode ser um sinônimo júnior desse táxon.',
+        periodo: "110 - 95 MA. Cretáceo",
+        link: "https://pt.wikipedia.org/wiki/Oxalaia",
+        imagem: "oxalaia.png"
+    },
+    {
+        titulo: "Pycnonemosaurus",
+        descricao: 'O picnonemossauro é considerado por alguns o maior membro da família Abelisauridae, sendo um dos maiores Terópodes descritos no Brasil.',
+        periodo: "94 - 84 MA. Cretáceo Superior",
+        link: "https://pt.wikipedia.org/wiki/Pycnonemosaurus"
+    },
+    {
+        titulo: "Unaysaurus",
+        descricao: "Foi o quarto dinossauro inequívoco descrito para as rochas do Triássico Sul-Brasileiro.",
+        periodo: "225 MA. Triássico",
+        link: "https://pt.wikipedia.org/wiki/Unaysaurus"
+    },
+    {
+        titulo: "Irritator",
+        descricao: " Um terópode piscívoro descoberto na Formação Romualdo, no Ceará. Seu nome deriva da frustração dos paleontólogos ao descobrir que o crânio havia sido adulterado por colecionadores.",
+        periodo: "110 MA. Cretáceo Inferior",
+        link: "https://pt.wikipedia.org/wiki/Irritator"
+    },
+    {
+        titulo: "Adamantisaurus",
+        descricao: "O Lagarto de Adamantina, é mais um Gênero de dinossauro Titanosaurídeo Brasileiro.",
+        periodo: "93 - 70 MA. Cretáceo Superior",
+        link: "https://pt.wikipedia.org/wiki/Adamantisaurus"
+    },
+    {
+        titulo: "Staurikosaurus",
+        descricao: "Um dos primeiros dinossauros a serem descobertos no Brasil, o Staurikosaurus era um carnívoro bípede que vivia no período Triássico. Ele era relativamente pequeno, mas já apresentava características que o ligavam aos grandes dinossauros que viriam depois." ,
+        periodo: "233,23 MA. Triássico Superior",
+        link: "https://pt.wikipedia.org/wiki/Staurikosaurus"
+    },
+    {
+        titulo: "Pampadromaeus:" ,
+        descricao: "dinossauro que habitou o Brasil no período Triássico, o Pampadromaeus era um herbívoro de tamanho médio. Seus fósseis foram encontrados na região de Santa Maria, no Rio Grande do Sul." ,
+        periodo: "233,23 MA. Triássico Superior",
+        link: "https://pt.wikipedia.org/wiki/Pampadromaeus"
+    },
+    {
+        titulo: "Uberabatitan",
+        descricao: "Um dos maiores titanossauros descobertos no Brasil, encontrado na região de Uberaba, Minas Gerais. Era herbívoro e de grande porte.",
+        periodo: "70-66 MA. Cretáceo Superior",
+        link: "https://pt.wikipedia.org/wiki/Uberabatitan",
+        imagem: "uberabatitan.png"
+    },
+    {
+        titulo: "Mosasaurus",
+        descricao: "Apesar de não ter sido encontrado diretamente no Brasil, fósseis de outros mosassaurídeos foram descobertos em várias partes do mundo, inclusive na América do Sul. Eles eram grandes répteis marinhos predadores, com corpos alongados e nadadeiras, dominando os oceanos no Cretáceo",
+        periodo: "80-66 MA. Cretáceo Superior",
+        link: "https://pt.wikipedia.org/wiki/Mosasaurus"
+    },
+    {
+        titulo: "Anhanguera",
+        descricao: "Um pterossauro brasileiro com asas longas e uma envergadura de até 4 metros, encontrado na Formação Santana, no Ceará. Possuía dentes adaptados para pegar peixes e crustáceos no ambiente marinho.",
+        periodo: "112-100 MA. Cretáceo Inferior",
+        link: "https://pt.wikipedia.org/wiki/Anhanguera_(g%C3%AAnero)",
+        imagem: "anhanguera.png"
+    },
+    {
+        titulo: "Prionosuchus",
+        descricao: "Um dos maiores anfíbios pré-históricos já descobertos, semelhante a um crocodilo gigante. Foi encontrado na Formação Pedra de Fogo, no Maranhão e no Tocantins. Podia chegar a 9 metros de comprimento, vivendo em ambientes aquáticos.",
+        periodo: "270-250 MA. Permiano",
+        link:"https://pt.wikipedia.org/wiki/Prionosuchus"
+    },
+    {
+        titulo: "Brasilosaurus",
+        descricao: "Um réptil aquático marinho pertencente ao grupo dos notossauros, que vivia nos mares rasos do Brasil pré-histórico. Esse grupo tinha corpos alongados e eram nadadores ágeis, predando pequenos peixes e invertebrados.",
+        periodo: "230-225 MA. Triássico",
+        link: "https://pt.wikipedia.org/wiki/Basilosaurus"
+    },
+    {
+        titulo: "Purussaurus",
+        descricao: "Um enorme crocodilo pré-histórico que viveu na América do Sul durante o Mioceno. Era um dos maiores crocodilídeos conhecidos, alcançando comprimentos de até 12 metros. O Purussaurus habitava rios e áreas alagadas, sendo um predador de topo que se alimentava de grandes presas.",
+        periodo: "8-5 MA. Mioceno",
+        link: "https://pt.wikipedia.org/wiki/Purussaurus"
+    }
+    
+
+
+
+];
+
+/*            
+            <div class="item-resultado">
+                <h2>
+                    <a href="https://rickandmorty.fandom.com/wiki/Morty_Smith" target="_blank">Morty Smith</a>
+                </h2>
+                <p class="descricao-meta">
+                    Morty Smith: O neto tímido e ansioso do Rick. Ele é arrastado pelas loucuras do avô e vive tendo que
+                    lidar com situações perigosas e absurdas. O Morty é um garoto normal que só quer viver uma vida
+                    tranquila, mas o Rick sempre tem um plano mirabolante para atrapalhar seus planos.
+                </p>
+                <a href="https://rickandmorty.fandom.com/wiki/Morty_Smith" target="_blank">mais informações</a>
+            </div>
+            <div class="item-resultado">
+                <h2>
+                    <a href="https://rickandmorty.fandom.com/wiki/Summer_Smith" target="_blank">Summer Smith</a>
+                </h2>
+                <p class="descricao-meta">
+                    Summer Smith: A irmã mais velha do Morty. Ela é popular na escola, adora selfies e se preocupa com a
+                    imagem que passa para os outros. A Summer é mais madura que o irmão, mas também é bastante
+                    superficial e preocupada com a aparência.
+                </p>
+                <a href="https://rickandmorty.fandom.com/wiki/Summer_Smith" target="_blank">mais informações</a>
+            </div>
+*/
